d2c197af91ec31f8997f1915e285c868874b34a1
ckehl <craigkehl@byui.edu>
2021-12-03 07:53:21 -0700

    reset to http

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
+import path from 'path'
+import fs, { readFileSync } from 'fs'
 import express, { Application } from 'express'
-// import path from 'path'
-// import session from 'express-session'
 import Cors from 'cors'
+
 import dotenv from 'dotenv'
 
 import userControlRoutes from './routes/userControl'
 
 const app: Application = express()
 
-// app.set('view engine', 'ejs')
-// app.set('views', 'views')
 app.use(Cors())
+app.use(dotenv.config)
+
+
+const privateKey = readFileSync(path.resolve(__dirname, './SSL/server.key'))
+const certificate = readFileSync(path.resolve(__dirname, './SSL/server.cert'))
 
 app.use(userControlRoutes)
 
