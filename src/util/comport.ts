4047cc7366e9d178d146e57d5986df2e27084b08
ckehl <codechck@gmail.com>
2022-08-21 11:25:33 -0600

    clean-up

--- src/util/comport.ts
+++ src/util/comport.ts
@@ -1,10 +1,7 @@
 import SerialPort from 'serialport';
 
-const CAM_PORT: string = process.env.CAM_SERIAL_PORT || '/dev/ttyUSB0)'
-
-
-
-// const port = new SerialPort('/dev/tty.usb0', {
+const CAM_PORT: string = process.env.CAM_SERIAL_PORT || 'COM8'
+// on mac /dev/ttyUSB0
 
 const port = new SerialPort(CAM_PORT, {
   baudRate: 9600,
@@ -15,15 +12,5 @@ const port = new SerialPort(CAM_PORT, {
 port.on('error', function (err) {
   console.log('Error: ', err.message);
 });
-
-// const port = new SerialPort('COM5', {
-//   baudRate: 9600,
-//   dataBits: 8,
-//   parity: 'none',
-//   stopBits: 1,
-// })
-// port.on('error', function (err) {
-//   console.log('Error: ', err.message)
-// })
 
 export default port;
