f44fde9f2d14f20fedf968ce621875a8602a6b53
Craig Kehl <craig@sportdev.us>
2024-06-13 00:15:38 -0600

    Remove unneeded import

--- src/Controllers/ptzCamera.ts
+++ src/Controllers/ptzCamera.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import PTZCamera from '../Models/PTZCamera';
 import { camPort  }from '../util/comport';
 
